8a7f141611532d943665231de7c42668d75a0e4a
gititub <61666085+gititub@users.noreply.github.com>
2023-10-03 16:28:35 +0200

    Update code.py

--- appNEN/code.py
+++ appNEN/code.py
@@ -18,7 +18,8 @@ def get_gene_info_by_gene_number(gene_numbers):
         data.append([number, word, sp, url])
 
     df = pd.DataFrame(data, columns=['gene', 'gene_name', 'sp', 'url'])
-    return df
+    df_cleaned = df.dropna(axis=1, how='all')
+    return df_cleaned
 
 
 def get_gene_info_by_gene_name(gene, species=None):
@@ -48,7 +49,8 @@ def get_gene_info_by_gene_name(gene, species=None):
             break  # Break the loop after finding the first entry for the specified species
 
     df = pd.DataFrame(data, columns=['gene_name', 'sp', 'id', 'url'])
-    return df
+    df_cleaned = df.dropna(axis=1, how='all')
+    return df_cleaned
 
 
 def get_gene_info_by_rsid(rsids):
@@ -67,12 +69,60 @@ def get_gene_info_by_rsid(rsids):
 
         data.append([rsid, sp, gene, url])
     df = pd.DataFrame(data, columns=['rsid', 'sp', 'gene:variant type', 'link'])
+    df_cleaned = df.dropna(axis=1, how='all')
+    return df_cleaned
+    
+
+def get_pmc_ids_from_pmids(file, input):
+    results = []
+    if file is not None and not file.empty:
+        pmids = file.iloc[:,0].drop_duplicates().tolist()
+    else:
+        pmids = [num.strip() for num in input.split(',') if num.strip()]
+    for pmid in pmids:
+        url = f"https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids={pmid}&format=json"
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            records = data.get("records", [])
+            if records:
+                record = records[0]
+                pmc_id = record.get("pmcid", None)
+                results.append((pmid, pmc_id))
+            else:
+                results.append((pmid, None))
+    df = pd.DataFrame(results, columns=["PMID", "PMCID"])
     return df
 
 
-def fetch_litvar_data(file):
-    file['litvar'] = file['gene'] + " " + file['HGVS']
-    query_list = file['litvar'].drop_duplicates().tolist()
+def get_pmids_from_pmc_ids(file, input):
+    results = []
+    if file is not None and not file.empty:
+        pmc_ids = file.iloc[:, 0].drop_duplicates().tolist()
+    else:
+        pmc_ids = [num.strip() for num in input.split(',') if num.strip()]
+    for pmc_id in pmc_ids:
+        url = f"https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids={pmc_id}&format=json"
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            records = data.get("records", [])
+            if records:
+                record = records[0]
+                pmid = record.get("pmid", None)
+                results.append((pmc_id, pmid))
+            else:
+                results.append((pmc_id, None))
+    df = pd.DataFrame(results, columns=["PMCID", "PMID"])
+    return df
+
+
+def fetch_litvar_data(file, input):
+    if file is not None and not file.empty:
+        file['litvar'] = file['gene'] + " " + file['HGVS']
+        query_list = file['litvar'].drop_duplicates().tolist()
+    else:
+        query_list = [num.strip() for num in input.split(',') if num.strip()]
     data_dict = {
         'Query': [],
         'rsid': [],
@@ -98,37 +148,8 @@ def fetch_litvar_data(file):
                 data_dict['data_clinical_significance'].append(result.get('data_clinical_significance', 'N/A'))
 
     litvar = pd.DataFrame(data_dict)
-    return litvar
-
-
-def litvar_data(input):
-    query_list = [num.strip() for num in input.split(',') if num.strip()]
-    data_dict = {
-        'Query': [],
-        'rsid': [],
-        'gene': [],
-        'name': [],
-        'hgvs': [],
-        'pmids_count': [],
-        'data_clinical_significance': []
-    }
-    for query in query_list:
-        url = f'https://www.ncbi.nlm.nih.gov/research/litvar2-api/variant/autocomplete/?query={query}'
-        response = requests.get(url)
-        data = response.json()
-
-        if data:
-            for result in data:
-                data_dict['Query'].append(query)
-                data_dict['rsid'].append(result.get('rsid', 'N/A'))
-                data_dict['gene'].append(result.get('gene', 'N/A'))
-                data_dict['name'].append(result.get('name', 'N/A'))
-                data_dict['hgvs'].append(result.get('hgvs', 'N/A'))
-                data_dict['pmids_count'].append(result.get('pmids_count', 'N/A'))
-                data_dict['data_clinical_significance'].append(result.get('data_clinical_significance', 'N/A'))
-
-    litvar = pd.DataFrame(data_dict)
-    return litvar
+    litvar_cleaned = litvar.dropna(axis=1, how='all')
+    return litvar_cleaned
 
 
 def synvar(input):
@@ -165,15 +186,22 @@ def synvar(input):
             drug_info = [f"{drug.get('preferred_term')}({drug.get('id')})" for drug in drug_data]
             data_dict['drugs'].append(drug_info)
 
+    pd.set_option('display.max_colwidth', 30)
     synvar_df = pd.DataFrame(data_dict)
-    return synvar_df
+    synvar_df['gene'] = synvar_df['gene'].apply(lambda x: ', '.join(x))
+    synvar_df['variants_synonyms'] = synvar_df['variants_synonyms'].apply(lambda x: ', '.join(x))
+    synvar_cleaned = synvar_df.dropna(axis=1, how='all')
+    return synvar_cleaned
 
 
 def synvar_file(file):
+    pd.set_option('display.max_colwidth', 30)
     file['query'] = file['gene'] + "(" + file['HGVS'] + ")"
     file['result'] = file.apply(fetch_data, axis=1)
     file['genes'] = file['result'].apply(extract_genes)
+    file['genes'] = file['genes'].apply(lambda x: ', '.join(x))
     file['variants_synonyms'] = file['result'].apply(extract_variants_syn)
+    file['variants_synonyms'] = file['variants_synonyms'].apply(lambda x: ', '.join(x))
     file['drugs'] = file['result'].apply(extract_drugs)
     return file[['pmid', 'query', 'genes', 'variants_synonyms', 'drugs']]
 
@@ -208,7 +236,6 @@ def extract_genes(result):
             return gene_info
         else:
             return None
-
 
 def extract_drugs(result):
     if result is None:
