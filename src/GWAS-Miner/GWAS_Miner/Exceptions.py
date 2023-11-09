cbaee545a3b7c9a80b4880d6962d1a629cbf3ebe
amelia <amartinezsequ@uoc.edu>
2023-11-09 14:13:55 +0100

    update src

--- src/GWAS-Miner/GWAS_Miner/Exceptions.py
+++
@@ -1,5 +0,0 @@
-class TableTypeError(Exception):
-    def __init__(self, table, message="Table type does not match row annotation layout"):
-        self.table = table
-        self.message = message
-        super().__init__(self.message)
